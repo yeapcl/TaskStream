3babec31fd9f2c022bf7c6bda2333b8573d177cc
Yeap Chun Lik <yeapchunlik@gmail.com>
2020-09-18 16:42:32 +0800

    printData

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -21,6 +21,8 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printData(tasksData);
+
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
